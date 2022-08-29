1d5a64bb89a38687cedaa2c82ba7bd8690a7b8fb
Koshal Raj Shakya <koshalshakya72@gmail.com>
2022-08-29 12:22:25 +0545

    setup firebase/firestore configs

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyAttTXHLjE8LJiIDBJUc04U8W16Vrux36s",
   authDomain: "todo-react-firebase-463cf.firebaseapp.com",
@@ -6,3 +8,8 @@ const firebaseConfig = {
   messagingSenderId: "412420388938",
   appId: "1:412420388938:web:cdf936020f579373100eb3"
 };
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export { db }
